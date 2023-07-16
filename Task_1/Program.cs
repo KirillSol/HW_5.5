073190425350dbb3ca50f5fc7dd01eb75babb9aa
Kirill Sobolev <sobolev_mdf119@mail.ru>
2023-07-16 17:30:22 +0300

    Добавьте файлы проекта.

---
+++ Task_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Разделяем текст на слова
+        /// </summary>
+        /// <param name="text">Введенное пользователем предложение</param>
+        /// <returns>Массив из разделенных слов</returns>
+        static string[] SplitText(string text)
+        {
+            string[] word = text.Split(' ');
+
+            foreach (var e in word)
+            {
+                Console.WriteLine($"{e}");
+            }
+            return word;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение: ");
+            SplitText(Console.ReadLine());
+        }
+    }
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string inputSentence = Console.ReadLine();
+
+            string reversedWords = ReverseWords(inputSentence);
+            Console.WriteLine("Перевернутые слова: " + reversedWords);
+
+        }
+
+        static string ReverseWords(string inputPhrase)
+        {
+            string[] words = SeparateWords(inputPhrase);
+            string reversedSentence = "";
+
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                reversedSentence += words[i] + " ";
+            }
+
+            return reversedSentence;
+        }
+
+        static string[] SeparateWords(string inputPhrase)
+        {
+            string[] words = inputPhrase.Split(' ');
+
+            return words;
+        }
+
+            
+
+        
+    }
+}
